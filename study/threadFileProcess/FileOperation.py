818880689c105f384f949e36fcdbd4a19650f6a8
cysuncn <cysuncn@126.com>
2017-08-15 00:08:11 +0800

    1

--- study/threadFileProcess/FileOperation.py
+++ study/threadFileProcess/FileOperation.py
@@ -1,15 +1,36 @@
 # -*- coding : utf-8 -*-
 from FileProcess import FileProcess
 from ConfigurationParser import ConfigurationParser
+import threadpool
+from NECIPS import NECIPS
+import time
 
 if __name__ == "__main__":
     config = ConfigurationParser("D:\\Git\\config\\", "conf.ini", "FileInfo")
     src_dir = config.get_config("src_dir")
     src_file = config.get_config("src_file")
+    tmp_dir = config.get_config("tmp_dir")
     src_filename = src_dir + src_file
-    clean_filename = "D://Git//result//test.txt"
+    clean_filename = tmp_dir + "clean.txt"
     des_dir = config.get_config("des_dir")
-    print(src_filename, des_dir)
+    log_dir = config.get_config("log_dir")
+    print(src_filename, des_dir, clean_filename)
     fo = FileProcess(src_filename, clean_filename, des_dir)
-    fo.get_clean_file(" ", 1)
-    fo.split_file(10000)
+    fo.get_clean_file("|", 0, False)
+    fo_list = fo.split_file(1)
+    # print(fo_list)
+    # n = NECIPS(fo_list[0], fo_list[0] + '_done', tmp_dir, des_dir + fo_list[0].split('\\')[-1],
+    #            log_dir + fo_list[0].split('\\')[-1])
+    # n.main()
+    t = []
+    for fname in fo_list:
+        print(fname)
+        t.append(
+            NECIPS(fname, fname + '_done', tmp_dir, des_dir + fname.split('\\')[-1],
+                   log_dir + fname.split('\\')[-1]))
+    start_time = time.time()
+    pool = threadpool.ThreadPool(1)
+    requests = threadpool.makeRequests(NECIPS.main, t)
+    [pool.putRequest(req) for req in requests]
+    pool.wait()
+    print('%d second' % (time.time() - start_time))
--- study/threadFileProcess/FileProcess.py
+++ study/threadFileProcess/FileProcess.py
@@ -1,7 +1,4 @@
 # -*- coding : utf-8 -*-
-import threading
-import os
-
 
 class FileProcess(object):
     def __init__(self, src_filename, clean_filename, des_dir):
@@ -17,33 +14,39 @@ class FileProcess(object):
             return False
         return True
 
-    def get_clean_file(self,  splitop, field):
+    def get_clean_file(self, splitop, field, flag):
         try:
             clean_file = open(self.clean_filename, 'w')
-            with open(self.src_filename, mode='rb') as reader:
+            with open(self.src_filename, mode = 'rb') as reader:
                 for line in reader:
                     data = line.decode('gbk', 'ignore').strip().split(splitop)
-                    if len(data) > 1:
-                        word = data[field]
-                        if self.isZh(word):
-                            clean_file.write("|".join(data) + '\n')
-                        else:
-                            pass
+                    if flag:
+                        if len(data) >= 1:
+                            word = data[field]
+                            if self.isZh(word):
+                                clean_file.write("|".join(data) + '\n')
+                            else:
+                                pass
+                    else:
+                        clean_file.write(line.decode('gbk', 'ignore').strip() + '\n')
         finally:
             clean_file.close()
 
     def split_file(self, lines):
-        with open(self.clean_filename, mode='rb') as reader:
+        file_list = []
+        with open(self.clean_filename, mode = 'rb') as reader:
             counter = 0
             findex = 0
             for line in reader:
                 data = line.decode('gbk', 'ignore').strip()
                 if counter == 0:
-                    writer = open(self.src_filename + str(findex), 'w')
+                    writer = open(self.src_filename + '_' + str(findex), 'w')
                 writer.write(data + '\n')
                 counter += 1
                 # print(counter)
                 if counter >= lines:
                     writer.close()
+                    file_list.append(self.src_filename + '_' + str(findex))
                     counter = 0
                     findex += 1
+        return file_list
