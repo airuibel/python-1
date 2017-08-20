2cdcf087c52d277dd584bbc2ed50668e09e33598
cysuncn <cysuncn@126.com>
2017-08-20 22:58:23 +0800

    1

--- spark/test-01.py
+++ spark/test-01.py
@@ -1,4 +1,21 @@
-import tensorflow as tf
-hello = tf.constant('Hello, TensorFlow!')
-sess = tf.Session()
-print(sess.run(hello))
+# -*- coding:utf8 -*-
+import networkx as nx
+import matplotlib.pyplot as plt
+
+G = nx.Graph()
+edgelist = []
+with open('F:\\test1.del', mode='rb') as f:
+    for line in f:
+        data = line.decode('gbk', 'ignore').strip().split("|")
+        if data[0] is not None and data[1] is not None and len(data[0]) > 0 and len(data[1]) > 0:
+            # print(data[0], data[1])
+            G.add_edge(data[0], data[1])
+            #edgelist.append((data[0], data[1]))
+
+# edgelist = [('a', 'c'), ('a', 'e'), ('b', 'a'), ('c', 'd'), ('d', 'b'), ('e', 'b')]
+#G.add_edges_from(edgelist)
+try:
+    nx.cycle_basis(G)
+except:
+    pass
+print(list(nx.cycle_basis(G)))
